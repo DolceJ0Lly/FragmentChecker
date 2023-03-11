235234018f10147f9c99893a3d2641a2976ec35a
FiokoLy's <78534917+DolceJ0Lly@users.noreply.github.com>
2023-03-11 10:27:01 +0100

    Changed the main.py from 🇮🇹(Italian) To 🇬🇧(English)
    From 🇮🇹 (Italian) to 🇬🇧 (English)

--- main.py
+++ main.py
@@ -1,73 +1,137 @@
 #   By GodAlecs
+
 #   GitHub: github.com/GodALecs/FragmentChecker
 
-#   Librerie
+#   Libs
+
 import requests
+
 import os
+
 import platform
+
 from colorama import Fore
 
-#   Variabili
-saves = "saves.txt" # Variabile del file Saves.txt
+#   Variables
+
+saves = "saves.txt" # Variable of the file Saves.txt
+
 link = "https://fragment.com/username/" # Link fragment
+
 banner = f"""
+
 {Fore.RED}▄████  █▄▄▄▄ ██     ▄▀  █▀▄▀█ ▄███▄      ▄     ▄▄▄▄▀ 
+
 █▀   ▀ █  ▄▀ █ █  ▄▀    █ █ █ █▀   ▀      █ ▀▀▀ █    
+
 █▀▀    █▀▀▌  █▄▄█ █ ▀▄  █ ▄ █ ██▄▄    ██   █    █    
+
 █      █  █  █  █ █   █ █   █ █▄   ▄▀ █ █  █   █     
+
 █       █      █  ███     █  ▀███▀   █  █ █  ▀      
+
 ▀     ▀      █          ▀           █   ██         
+
             ▀                                      
+
 {Fore.YELLOW}▄█▄     ▄  █ ▄███▄   ▄█▄    █  █▀ ▄███▄   █▄▄▄▄      
+
 █▀ ▀▄  █   █ █▀   ▀  █▀ ▀▄  █▄█   █▀   ▀  █  ▄▀      
+
 █   ▀  ██▀▀█ ██▄▄    █   ▀  █▀▄   ██▄▄    █▀▀▌       
+
 █▄  ▄▀ █   █ █▄   ▄▀ █▄  ▄▀ █  █  █▄   ▄▀ █  █       
+
 ▀███▀     █  ▀███▀   ▀███▀    █   ▀███▀     █        
+
         ▀                   ▀             ▀     
+
         
+
     {Fore.RED}github.com/GodAlecs/FragmentChecker
+
     """ #   Banner
 
 #   Funzione check_user
+
 def check_user(username): 
+
     username = username.strip()
-    r = requests.get(f"https://fragment.com/username/{username}", allow_redirects=False) #  Controlla se l'username è in asta
+
+    r = requests.get(f"https://fragment.com/username/{username}", allow_redirects=False) #  Check if the user is on shop
+
     response = str(r.text).split('\n')
+
     if len(response) != 1:
-        print(f"{Fore.GREEN}[+]{Fore.RESET} {username} è possibile acquistarlo su fragment.com")
+
+        print(f"{Fore.GREEN}[+]{Fore.RESET} {username} can be buy it on Fragment.com")
+
     else:
-        print(f"{Fore.RED}[-]{Fore.RESET} {username} non è possibile acquistarlo su fragment.com")
-        #   Qui sotto salva gli username NON in asta in un file
+
+        print(f"{Fore.RED}[-]{Fore.RESET} {username} can't be buy on fragment.com")
+
+        #   Down saves the username the can't be buyed
+
         saves_file = open(saves, "a")
+
         saves_file.write(f"[-] {username}\n")
+
         saves_file.close()
 
-#   Funzione Main
+#   Function Main
+
 def main():
-    if "linux" in str(platform.platform()).lower(): #   Cancella le scritte sullo schermo
+
+    if "linux" in str(platform.platform()).lower(): # Wash the screen (remove the writing)
+
         os.system("clear")
+
     else: 
+
         os.system("cls")
+
     print(banner)
-    wordlist = input(f"{Fore.RESET}[>>] Inserire il nome del file {Fore.GREEN}wordlist{Fore.RESET} -> ") #  Inserire nome della wordlist
-    try: #  Vede se esiste la wordlist
+
+    wordlist = input(f"{Fore.RESET}[>>] Enter the name of the file of the  {Fore.GREEN}wordlist{Fore.RESET} -> ") #  Enter the name
+
+    try: #  Check if exist the file...
+
         wordlist_file = open(wordlist, "r")
-        print(f"{Fore.RESET}[>>] File {wordlist} {Fore.GREEN}caricato{Fore.RESET}!")
-        try: #  Vede se esiste il file che salva gli username non in asta
+
+        print(f"{Fore.RESET}[>>] File {wordlist} {Fore.GREEN}Loaded!{Fore.RESET}!")
+
+        try: #  Check if the file is exist for the name can't buy on fragment.com
+
             saves_file = open(saves, "r+")
-            print(f"{Fore.RESET}[>>] File {saves} {Fore.GREEN}caricato{Fore.RESET}!")
-            saves_file.truncate(0) #    Cancella tutti i dati
-            saves_file.close() #   Chiude il file Saves.txt
+
+            print(f"{Fore.RESET}[>>] File {saves} {Fore.GREEN}Loaded!{Fore.RESET}!")
+
+            saves_file.truncate(0) #    Delete all data
+
+            saves_file.close() #   Close the file Saves.txt
+
             pass
-        except FileNotFoundError: # Crea il file Saves.txt
+
+        except FileNotFoundError: # Create file saves.txt
+
             saves_file = open(saves, "w")
-            print(f"{Fore.RESET}[>>] File {saves} {Fore.GREEN}creato{Fore.RESET}!")
+
+            print(f"{Fore.RESET}[>>] File {saves} {Fore.GREEN}Crated{Fore.RESET}!")
+
             saves_file.close()
-    except FileNotFoundError: # File wordlist non trovato
-        print(f"{Fore.RESET}[>>] File {wordlist} {Fore.RED}non trovato{Fore.RESET}!")
-        exit() #    Finisce il programma
-    print() #   Spazio vuoto
+
+    except FileNotFoundError: # File wordlist not found
+
+        print(f"{Fore.RESET}[>>] File {wordlist} {Fore.RED}Not found{Fore.RESET}!")
+
+        exit() #    Finish the program
+
+    print() #   Blank Space
+
     for username in wordlist_file.readlines():
+
         check_user(username.lower())
+
 if __name__ == "__main__":
-    main() #    Richiama classe Main
+
+    main() #    Recall Class 
